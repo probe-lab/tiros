a2082423dec8fcdccbf9965f284d4ff7ab53c3c4
Dennis Trautwein <git@dtrautwein.eu>
2023-03-28 10:02:53 +0200

    enrich existing data with metrics

--- migrations/000003_alter_measurements_table.up.sql
+++ migrations/000003_alter_measurements_table.up.sql
@@ -22,5 +22,31 @@ ALTER TABLE measurements
 ALTER TABLE measurements
     ADD COLUMN lcp_rating rating;
 
+UPDATE measurements
+SET ttfb_rating = CASE -- https://web.dev/ttfb/#what-is-a-good-ttfb-score
+        WHEN ttfb < '0.8 seconds' THEN
+            'GOOD'
+        WHEN ttfb < '1.8 seconds' THEN
+            'NEEDS_IMPROVEMENT'
+        ELSE
+            'POOR'
+    END::rating,
+    fcp_rating = CASE -- https://web.dev/fcp/#what-is-a-good-fcp-score
+        WHEN fcp < '1.8 seconds' THEN
+            'GOOD'
+        WHEN fcp < '3 seconds' THEN
+            'NEEDS_IMPROVEMENT'
+        ELSE
+            'POOR'
+    END::rating,
+    lcp_rating = CASE -- https://web.dev/lcp/#what-is-a-good-lcp-score
+        WHEN fcp < '2.5 seconds' THEN
+            'GOOD'
+        WHEN fcp < '4 seconds' THEN
+            'NEEDS_IMPROVEMENT'
+        ELSE
+            'POOR'
+    END::rating;
+
 COMMIT;
 
