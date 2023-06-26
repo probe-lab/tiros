38446ef2fd2af3a31d7bd127b0def43aca25938b
Dennis Trautwein <git@dtrautwein.eu>
2023-06-26 13:14:17 +0200

    refactor: website probing

--- probe.go
+++ probe.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/go-rod/rod"
@@ -24,6 +23,76 @@ var (
 	ErrOnLoadTimeout      = errors.New("window.onload event timed out")
 	ErrNetworkIdleTimeout = errors.New("window.requestIdleCallback timed out")
 )
+
+func (pr *probeResult) NullError() null.String {
+	if pr.err != nil {
+		return null.StringFrom(pr.err.Error())
+	}
+	return null.NewString("", false)
+}
+
+func (t *tiros) measureWebsites(c *cli.Context, websites []string, results chan<- *probeResult) {
+	defer close(results)
+
+	for j, settle := range c.IntSlice("settle-times") {
+
+		sleepDur := time.Duration(settle) * time.Second
+
+		log.Infof("Letting Kubo settle for %s\n", sleepDur)
+		time.Sleep(sleepDur)
+
+		for i := 0; i < c.Int("times"); i++ {
+			for _, mType := range []string{models.MeasurementTypeKUBO, models.MeasurementTypeHTTP} {
+				for _, website := range websites {
+
+					pr, err := newProbe(c, website, mType).run()
+					if errors.Is(c.Context.Err(), context.Canceled) {
+						return
+					} else if err != nil {
+						log.WithError(err).WithField("website", website).Warnln("error probing website")
+						continue
+					}
+
+					pr.website = website
+					pr.mType = mType
+					pr.try = i + j*len(c.IntSlice("settle-times"))
+
+					log.WithFields(log.Fields{
+						"ttfb":   p2f(pr.ttfb),
+						"lcp":    p2f(pr.lcp),
+						"fcp":    p2f(pr.fcp),
+						"tti":    p2f(pr.tti),
+						"status": pr.httpStatus,
+					}).WithError(pr.err).Infoln("Probed website", website)
+
+					results <- pr
+
+					if mType == models.MeasurementTypeKUBO {
+						if err = t.KuboGC(c.Context); err != nil {
+							log.WithError(err).Warnln("error running kubo gc")
+							continue
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+type probe struct {
+	ctx     context.Context
+	url     string
+	website string
+	mType   string
+	cdpPort int
+
+	browser *rod.Browser
+	page    *rod.Page
+
+	result          *probeResult
+	metricsStr      string
+	navPerfEntryStr string
+}
 
 type probeResult struct {
 	url     string
@@ -56,247 +125,206 @@ type probeResult struct {
 	err error
 }
 
-func (pr *probeResult) NullError() null.String {
-	if pr.err != nil {
-		return null.StringFrom(pr.err.Error())
-	}
-	return null.NewString("", false)
-}
-
-func (t *tiros) measureWebsites(c *cli.Context, websites []string, results chan<- *probeResult) {
-	defer close(results)
-
-	for j, settle := range c.IntSlice("settle-times") {
-
-		sleepDur := time.Duration(settle) * time.Second
-
-		log.Infof("Letting Kubo settle for %s\n", sleepDur)
-		time.Sleep(sleepDur)
-
-		for i := 0; i < c.Int("times"); i++ {
-			for _, mType := range []string{models.MeasurementTypeKUBO, models.MeasurementTypeHTTP} {
-				for _, website := range websites {
-					pr, err := t.probeWebsite(c, websiteURL(c, website, mType))
-					if errors.Is(c.Context.Err(), context.Canceled) {
-						return
-					} else if err != nil {
-						log.WithError(err).WithField("website", website).Warnln("error probing website")
-						continue
-					}
-
-					pr.website = website
-					pr.mType = mType
-					pr.try = i + j*len(c.IntSlice("settle-times"))
-
-					log.WithFields(log.Fields{
-						"ttfb": p2f(pr.ttfb),
-						"lcp":  p2f(pr.lcp),
-						"fcp":  p2f(pr.fcp),
-						"tti":  p2f(pr.tti),
-					}).WithError(pr.err).Infoln("Probed website", website)
-
-					results <- pr
-
-					if mType == models.MeasurementTypeKUBO {
-						if err = t.KuboGC(c.Context); err != nil {
-							log.WithError(err).Warnln("error running kubo gc")
-							continue
-						}
-					}
-				}
-			}
-		}
+func newProbe(c *cli.Context, website string, mType string) *probe {
+	return &probe{
+		ctx:     c.Context,
+		url:     websiteURL(c, website, mType),
+		website: website,
+		mType:   mType,
+		cdpPort: c.Int("chrome-cdp-port"),
+		result: &probeResult{
+			url:     websiteURL(c, website, mType),
+			website: website,
+			mType:   mType,
+		},
 	}
 }
 
-func (t *tiros) probeWebsite(c *cli.Context, url string) (*probeResult, error) {
-	logEntry := log.WithField("url", url)
+func (p *probe) logEntry() *log.Entry {
+	return log.WithField("url", p.url)
+}
 
-	logEntry.Infoln("Probing website...")
-	defer logEntry.Infoln("Done probing!")
-
-	// Initialize the probe result that we'll hand back
-	pr := probeResult{
-		url: url,
+func (p *probe) run() (*probeResult, error) {
+	if err := p.initBrowser(); err != nil {
+		return nil, err
 	}
+	defer p.close()
 
-	// Initialize browser reference
-	browser := rod.New().
-		Context(c.Context). // stop when outer ctx stops
-		ControlURL(fmt.Sprintf("ws://localhost:%d", c.Int("chrome-cdp-port")))
-
-	// Connecting to headless chrome
-	logEntry.Debugln("Connecting to browser...")
-	if err := browser.Connect(); err != nil {
-		return nil, fmt.Errorf("connecting to browser: %w", err)
-	}
-	defer func() {
-		logEntry.Debugln("Closing connection to browser...")
-		if err := browser.Close(); err != nil && !errors.Is(err, context.Canceled) {
-			logEntry.WithError(err).Warnln("Error closing browser")
-		}
-	}()
-
-	// Initialize hijack results chanel
-	hijackChan := make(chan *rod.HijackResponse)
-	defer close(hijackChan)
-
-	// Initialize request hijacking
-	router := browser.HijackRequests()
-	go router.Run()
-	defer func() {
-		if err := router.Stop(); err != nil && !errors.Is(err, context.Canceled) {
-			logEntry.WithError(err).Warnln("Failed stopping request hijack router")
-		}
-	}()
-
-	hijackHandler := func(hijack *rod.Hijack) {
-		if err := hijack.LoadResponse(http.DefaultClient, true); err != nil {
-			logEntry.WithError(err).Warnln("Failed loading response")
-			return
-		}
-
-		go func() {
-			// this context will be cancelled when router.Stop() is called.
-			// After that, it's safe to close hijackChan.
-			ctx := hijack.Request.Req().Context()
-			select {
-			case hijackChan <- hijack.Response:
-			case <-ctx.Done():
-			}
-		}()
-	}
-
-	var metricsStr string
-	var navPerfEntryStr string
 	err := rod.Try(func() {
-		// register hijack handlers
-		router.MustAdd(url, hijackHandler)     // IPNS requests match here
-		router.MustAdd(url+"/", hijackHandler) // https requests match here
-
-		logEntry.Debugln("Initialize incognito browser")
-		browser = browser.Context(c.Context).MustIncognito() // first defense to prevent hitting the cache
-
-		logEntry.Debugln("Clearing browser cookies") // empty arguments clear cookies
-		browser.MustSetCookies()                     // second defense to prevent hitting the cache
-
-		logEntry.Debugln("Opening new page")
-		page := browser.MustPage()
-
-		// clear local storage and attaching onerror listeners
-		logEntry.Debugln("Attaching javascript")
-		page.MustEvalOnNewDocument(jsOnNewDocument) // third defense to prevent hitting the cache
-
-		// disable cache
-		err := proto.NetworkSetCacheDisabled{CacheDisabled: true}.Call(page) // fourth defense to prevent hitting the cache
-		if err != nil {
-			panic(err)
-		}
-
-		logEntry.WithField("timeout", websiteRequestTimeout).Debugln("Navigating...")
-		err = page.Timeout(websiteRequestTimeout).Navigate(url)
-		if errors.Is(err, context.DeadlineExceeded) {
-			panic(ErrNavigateTimeout)
-		} else if err != nil {
-			panic(err)
-		}
-
-		logEntry.WithField("timeout", websiteRequestTimeout).Debugln("Waiting for onload event ...")
-		// load: fired when the whole page has loaded (including all dependent resources such as stylesheets, scripts, iframes, and images)
-		err = page.Timeout(websiteRequestTimeout).WaitLoad()
-		if errors.Is(err, context.Canceled) {
-			panic(err)
-		} else if err != nil {
-			pr.err = ErrOnLoadTimeout
-		}
-
-		logEntry.WithField("timeout", websiteRequestTimeout).Debugln("Waiting for network idle event ...")
-		// idle: fired when network has come to a halt (1 Minute)
-		err = page.Timeout(websiteRequestTimeout).WaitIdle(time.Minute)
-		if errors.Is(err, context.Canceled) {
-			panic(err)
-		} else if err != nil {
-			if pr.err != nil {
-				err = fmt.Errorf("%s: %w", ErrNetworkIdleTimeout.Error(), pr.err)
-			}
-			pr.err = ErrNetworkIdleTimeout
-		}
-
-		logEntry.Debugln("Running polyfill JS ...")
-		page.MustEval(wrapInFn(jsTTIPolyfill))   // add TTI polyfill
-		page.MustEval(wrapInFn(jsWebVitalsIIFE)) // web-vitals
-
-		logEntry.Debugln("Running measurement ...")
-		metricsStr = page.MustEval(jsMeasurement).Str() // finally actually measure the stuff
-
-		logEntry.Debugln("Getting Performance Entry measurement ...")
-		rro, err := page.Eval(jsNavPerfEntry)
-		if err != nil {
-			logEntry.WithError(err).Warnln("Couldn't get navigation performance entry")
-		} else {
-			navPerfEntryStr = rro.Value.Str()
-		}
-
-		page.MustClose()
+		p.mustInitPage()
+		p.mustNavigate()
+		p.mustMeasure()
 	})
 
-	select {
-	case hijackResponse := <-hijackChan:
-		pr.httpStatus = hijackResponse.Payload().ResponseCode
-		pr.httpBody = hijackResponse.Body()
-	default:
-		logEntry.Warnln("No hijack entry consumed")
-	}
-
 	if errors.Is(err, ErrNavigateTimeout) {
-		logEntry.WithError(ErrNavigateTimeout).Warnln("Couldn't measure website performance.")
-		pr.err = ErrNavigateTimeout
-		return &pr, nil
+		p.logEntry().WithError(ErrNavigateTimeout).Warnln("Couldn't measure website performance.")
+		p.result.err = ErrNavigateTimeout
+		return p.result, nil
 	} else if errors.Is(err, context.Canceled) {
 		return nil, err
 	} else if err != nil {
-		logEntry.WithError(err).Warnln("Couldn't measure website performance.")
-		pr.err = err
-		return &pr, nil
+		p.logEntry().WithError(err).Warnln("Couldn't measure website performance.")
+		p.result.err = err
+		return p.result, nil
 	}
 
-	vitals := WebVitals{}
-	if err := json.Unmarshal([]byte(metricsStr), &vitals); err != nil {
-		return nil, fmt.Errorf("unmarshal web-vitals: %w", err)
+	return p.result, p.parseMetrics()
+}
+
+func (p *probe) initBrowser() error {
+	// Initialize browser reference
+	p.browser = rod.New().
+		Context(p.ctx). // stop when outer ctx stops
+		ControlURL(fmt.Sprintf("ws://localhost:%d", p.cdpPort))
+
+	// Connecting to headless chrome
+	p.logEntry().Debugln("Connecting to browser...")
+	if err := p.browser.Connect(); err != nil {
+		return fmt.Errorf("connecting to browser: %w", err)
 	}
 
-	if navPerfEntryStr != "" {
-		navPerfEntry := PerformanceNavigationEntry{}
-		if err := json.Unmarshal([]byte(navPerfEntryStr), &navPerfEntry); err != nil {
-			return nil, fmt.Errorf("unmarshal navigation performance entry: %w", err)
+	return nil
+}
+
+func (p *probe) mustInitPage() {
+	p.logEntry().Debugln("Initialize incognito browser")
+	// first defense to prevent hitting the cache.
+	// use an incognito browser
+	browser := p.browser.Context(p.ctx).MustIncognito()
+
+	p.logEntry().Debugln("Clearing browser cookies")
+	// second defense to prevent hitting the cache
+	// empty arguments clear cookies
+	browser.MustSetCookies()
+
+	p.logEntry().Debugln("Opening new page")
+	p.page = browser.MustPage()
+
+	p.logEntry().Debugln("Attaching javascript")
+	// third defense to prevent hitting the cache
+	// clear local storage and attaching onerror listeners
+	p.page.MustEvalOnNewDocument(jsOnNewDocument)
+
+	// disable cache
+	err := proto.NetworkSetCacheDisabled{CacheDisabled: true}.Call(p.page) // fourth defense to prevent hitting the cache
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (p *probe) mustNavigate() {
+	e := proto.NetworkResponseReceived{}
+	wait := p.page.Timeout(websiteRequestTimeout).WaitEvent(&e)
+
+	p.logEntry().WithField("timeout", websiteRequestTimeout).Debugln("Navigating...")
+	err := p.page.Timeout(websiteRequestTimeout).Navigate(p.url)
+
+	wait()
+
+	if e.Type == proto.NetworkResourceTypeDocument {
+		p.result.httpStatus = e.Response.Status
+		if p.result.httpStatus < 200 || p.result.httpStatus >= 300 {
+			p.result.httpBody = p.page.MustElement("body").MustText()
 		}
-		pr.navPerf = &navPerfEntry
+	}
+
+	if errors.Is(err, context.DeadlineExceeded) {
+		panic(ErrNavigateTimeout)
+	} else if err != nil {
+		panic(err)
+	}
+
+	p.logEntry().WithField("timeout", websiteRequestTimeout).Debugln("Waiting for onload event ...")
+	// load: fired when the whole page has loaded (including all dependent resources such as stylesheets, scripts, iframes, and images)
+	err = p.page.Timeout(websiteRequestTimeout).WaitLoad()
+	if errors.Is(err, context.Canceled) {
+		panic(err)
+	} else if err != nil {
+		p.result.err = ErrOnLoadTimeout
+	}
+
+	p.logEntry().WithField("timeout", websiteRequestTimeout).Debugln("Waiting for network idle event ...")
+	// idle: fired when the network has come to a halt (1 Minute)
+	err = p.page.Timeout(websiteRequestTimeout).WaitIdle(time.Minute)
+	if errors.Is(err, context.Canceled) {
+		panic(err)
+	} else if err != nil {
+		if p.result.err != nil {
+			err = fmt.Errorf("%s: %w", ErrNetworkIdleTimeout.Error(), p.result.err)
+		}
+		p.result.err = ErrNetworkIdleTimeout
+	}
+}
+
+func (p *probe) mustMeasure() {
+	p.logEntry().Debugln("Running polyfill JS ...")
+	p.page.MustEval(wrapInFn(jsTTIPolyfill))   // add TTI polyfill
+	p.page.MustEval(wrapInFn(jsWebVitalsIIFE)) // web-vitals
+
+	p.logEntry().Debugln("Running measurement ...")
+	p.metricsStr = p.page.MustEval(jsMeasurement).Str() // finally measure the stuff
+
+	p.logEntry().Debugln("Getting Performance Entry measurement ...")
+	rro, err := p.page.Eval(jsNavPerfEntry)
+	if err != nil {
+		p.logEntry().WithError(err).Warnln("Couldn't get navigation performance entry")
+	} else {
+		p.navPerfEntryStr = rro.Value.Str()
+	}
+}
+
+func (p *probe) parseMetrics() error {
+	vitals := WebVitals{}
+	if err := json.Unmarshal([]byte(p.metricsStr), &vitals); err != nil {
+		return fmt.Errorf("unmarshal web-vitals: %w", err)
+	}
+
+	if p.navPerfEntryStr != "" {
+		navPerfEntry := PerformanceNavigationEntry{}
+		if err := json.Unmarshal([]byte(p.navPerfEntryStr), &navPerfEntry); err != nil {
+			return fmt.Errorf("unmarshal navigation performance entry: %w", err)
+		}
+		p.result.navPerf = &navPerfEntry
 	}
 
 	for _, v := range vitals {
 		v := v
 		switch v.Name {
 		case "LCP":
-			pr.lcp = &v.Value
-			pr.lcpRating = &v.Rating
+			p.result.lcp = &v.Value
+			p.result.lcpRating = &v.Rating
 		case "FCP":
-			pr.fcp = &v.Value
-			pr.fcpRating = &v.Rating
+			p.result.fcp = &v.Value
+			p.result.fcpRating = &v.Rating
 		case "TTFB":
-			pr.ttfb = &v.Value
-			pr.ttfbRating = &v.Rating
+			p.result.ttfb = &v.Value
+			p.result.ttfbRating = &v.Rating
 		case "TTI":
-			pr.tti = &v.Value
-			pr.ttiRating = &v.Rating
+			p.result.tti = &v.Value
+			p.result.ttiRating = &v.Rating
 		case "CLS":
-			pr.cls = &v.Value
-			pr.clsRating = &v.Rating
+			p.result.cls = &v.Value
+			p.result.clsRating = &v.Rating
 		default:
 			continue
 		}
 	}
 
-	return &pr, nil
+	return nil
+}
+
+func (p *probe) close() {
+	p.logEntry().Debugln("Closing page...")
+	if p.page != nil {
+		if err := p.page.Close(); err != nil && !errors.Is(err, context.Canceled) {
+			p.logEntry().WithError(err).Warnln("Error closing page")
+		}
+	}
+
+	p.logEntry().Debugln("Closing browser connection...")
+	if err := p.browser.Close(); err != nil && !errors.Is(err, context.Canceled) {
+		p.logEntry().WithError(err).Warnln("Error closing browser connection")
+	}
 }
 
 func websiteURL(c *cli.Context, website string, mType string) string {
