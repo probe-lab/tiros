5e58211a3922c08fc9b7447bb8b5ddddd7f6e989
Dennis Trautwein <git@dtrautwein.eu>
2026-01-16 18:43:20 +0100

    fix: excessive CID select queries

--- cid_provider.go
+++ cid_provider.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multicodec"
 	pllog "github.com/probe-lab/go-commons/log"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 )
 
 type CIDProvider interface {
@@ -43,7 +45,8 @@ func (p *StaticCIDProvider) SelectCID(ctx context.Context, origin string) (cid.C
 }
 
 type BitswapSnifferClickhouseCIDProvider struct {
-	conn driver.Conn
+	conn             driver.Conn
+	cidSelectCounter metric.Int64Counter
 }
 
 var _ CIDProvider = (*BitswapSnifferClickhouseCIDProvider)(nil)
@@ -54,14 +57,27 @@ func NewBitswapSnifferClickhouseCIDProvider(dbClient DBClient) (*BitswapSnifferC
 		return nil, fmt.Errorf("expected clickhouse client, got: %T", dbClient)
 	}
 
-	return &BitswapSnifferClickhouseCIDProvider{conn: chClient.conn}, nil
+	meter := otel.GetMeterProvider().Meter("tiros")
+	cidSelectCounter, err := meter.Int64Counter("cid_select")
+	if err != nil {
+		return nil, fmt.Errorf("creating cid select counter: %w", err)
+	}
+
+	return &BitswapSnifferClickhouseCIDProvider{
+		conn:             chClient.conn,
+		cidSelectCounter: cidSelectCounter,
+	}, nil
 }
 
 func (p *BitswapSnifferClickhouseCIDProvider) SelectCID(ctx context.Context, origin string) (cid.Cid, error) {
 	msgType := "%"
+	limit := 100
+
 	if origin == "dht" {
 		msgType = "add-provider-records"
+		limit = 1
 	}
+
 	rows, err := p.conn.Query(ctx, `
 		WITH cte AS (
 			SELECT
@@ -70,32 +86,37 @@ func (p *BitswapSnifferClickhouseCIDProvider) SelectCID(ctx context.Context, ori
 			WHERE origin = $1
 			  AND msg_type LIKE $2
 			ORDER BY timestamp DESC
-			LIMIT 500
+			LIMIT $3
 		) SELECT cid FROM cte ORDER BY RAND() LIMIT 1
-	`, origin, msgType)
+	`, origin, msgType, limit)
+
+	p.cidSelectCounter.Add(ctx, 1, metric.WithAttributes(
+		attribute.String("origin", origin),
+		attribute.Bool("success", err == nil && rows.Err() == nil),
+	))
+
+	var c cid.Cid
 	if err != nil {
-		return cid.Cid{}, err
+		return c, err
 	}
 	defer pllog.Defer(rows.Close, "Failed closing rows")
 
-	if !rows.Next() {
-		return cid.Cid{}, sql.ErrNoRows
+	if rows.Next() {
+		var cidStr string
+		if err := rows.Scan(&cidStr); err != nil {
+			return c, err
+		}
+
+		c, err = cid.Parse(cidStr)
+		if err != nil {
+			return c, err
+		}
+
+		// tmp fix until https://github.com/probe-lab/bitswap-sniffer/pull/11 is merged
+		if c.Prefix().Codec != uint64(multicodec.DagPb) {
+			c = cid.NewCidV1(uint64(multicodec.Raw), c.Hash())
+		}
 	}
 
-	var cidStr string
-	if err := rows.Scan(&cidStr); err != nil {
-		return cid.Cid{}, err
-	}
-
-	c, err := cid.Parse(cidStr)
-	if err != nil {
-		return cid.Cid{}, err
-	}
-
-	// tmp fix until https://github.com/probe-lab/bitswap-sniffer/pull/11 is merged
-	if c.Prefix().Codec != uint64(multicodec.DagPb) {
-		return cid.NewCidV1(uint64(multicodec.Raw), c.Hash()), nil
-	}
-
-	return c, nil
+	return c, rows.Err()
 }
